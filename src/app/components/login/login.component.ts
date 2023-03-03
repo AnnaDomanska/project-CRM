47f6507fa6c3ed1a8544bbbef4633ad00544f639
annah <anna.h.domanska@gmail.com>
2023-03-03 14:24:15 +0100

    feat: login layout added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,10 +4,11 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.css'],
   templateUrl: './login.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +18,15 @@ export class LoginComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   });
+
+  constructor(private _authService: AuthService) {}
+
+  onLoginFormSubmitted(loginForm: FormGroup): void {
+    this._authService
+      .login({
+        email: loginForm.value.email,
+        password: loginForm.value.password,
+      })
+      .subscribe();
+  }
 }
