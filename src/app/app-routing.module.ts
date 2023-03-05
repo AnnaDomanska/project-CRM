31394190f781bf27f930be35305f259cc6c80028
annah <anna.h.domanska@gmail.com>
2023-03-05 12:43:25 +0100

    feat: verify added to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
 import { LoginComponentModule } from './components/login/login.component-module';
 import { RegisterComponentModule } from './components/register/register.component-module';
+import { VerifyComponentModule } from './components/verify/verify.component-module';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([{ path: 'auth/login', component: LoginComponent }, { path: 'auth/register', component: RegisterComponent }]),
+    RouterModule.forRoot([{ path: 'auth/login', component: LoginComponent }, { path: 'auth/register', component: RegisterComponent }, { path: 'verify', component: VerifyComponent }]),
     LoginComponentModule,
-    RegisterComponentModule
+    RegisterComponentModule,
+    VerifyComponentModule
   ],
   exports: [RouterModule],
 })
