3b1a75e44c3b922ebae8b5786a2360116791b7ec
annah <anna.h.domanska@gmail.com>
2023-03-05 22:00:18 +0100

    feat: bio component added to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { VerifyComponent } from './components/verify/verify.component';
 import { LeadsComponent } from './components/leads/leads.component';
+import { BioComponent } from './components/bio/bio.component';
 import { LoginComponentModule } from './components/login/login.component-module';
 import { RegisterComponentModule } from './components/register/register.component-module';
 import { VerifyComponentModule } from './components/verify/verify.component-module';
 import { LeadsComponentModule } from './components/leads/leads.component-module';
-import { VerifyGuard } from './guards/verify/verify.guard';
+import { BioComponentModule } from './components/bio/bio.component-module';
 
 @NgModule({
   imports: [
@@ -16,18 +17,15 @@ import { VerifyGuard } from './guards/verify/verify.guard';
       { path: 'auth/login', component: LoginComponent },
       { path: 'auth/register', component: RegisterComponent },
       { path: 'verify', component: VerifyComponent },
-      {
-        path: 'leads',
-        data: { redirectUrlEmail: 'verify' },
-        canActivate: [VerifyGuard],
-        component: LeadsComponent,
-      },
+      { path: 'leads', component: LeadsComponent },
+      { path: 'complete-profile', component: BioComponent }
     ]),
     LoginComponentModule,
     RegisterComponentModule,
     VerifyComponentModule,
     LeadsComponentModule,
+    BioComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
