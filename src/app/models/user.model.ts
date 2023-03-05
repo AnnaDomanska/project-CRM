72580ac228c98fe5fbf3b9c9d45af53e827c1e82
annah <anna.h.domanska@gmail.com>
2023-03-05 12:58:45 +0100

    comit: user model created

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,22 @@
+export interface UserModel {
+  readonly iss: string;
+  readonly aud: string;
+  readonly auth_time: number;
+  readonly user_id: string;
+  readonly sub: string;
+  readonly iat: number;
+  readonly exp: number;
+  readonly email: string;
+  readonly email_verified: string;
+  readonly firebase: UserFirebase;
+  readonly uid: string;
+}
+
+interface UserFirebase {
+  readonly identities: FirebaseIndentities;
+  readonly sign_in_provider: string;
+}
+
+interface FirebaseIndentities {
+  readonly email: string;
+}
