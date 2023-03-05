715031ebeb2a2c93ff05188aab43af65b0b80f0c
annah <anna.h.domanska@gmail.com>
2023-03-05 21:10:50 +0100

    feat: auth interceptor implemented

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,16 +3,31 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private _authService: AuthService) {}
 
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const blackList = ['auth/login'];
+    if (blackList.find((url) => request.url.endsWith(url))) {
+      return next.handle(request);
+    } else {
+      return this._authService.accessToken$.pipe(
+        switchMap((token) => {
+          const newRequest = request.clone({
+            setHeaders: { Authorizaton: `Bearer ${token}` },
+          });
+          return next.handle(newRequest);
+        })
+      );
+    }
   }
 }
