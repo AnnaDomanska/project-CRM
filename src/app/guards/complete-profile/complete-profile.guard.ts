2eed90fa919be58eefc793044b93e4098ddeb9d5
annah <anna.h.domanska@gmail.com>
2023-03-05 22:06:29 +0100

    fix: complete-profile guard fixed

--- src/app/guards/complete-profile/complete-profile.guard.ts
+++ src/app/guards/complete-profile/complete-profile.guard.ts
@@ -18,11 +18,12 @@ export class CompleteProfileGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> {
+    console.log('complete-profile guard activated');
     return this._userService.getUserBio().pipe(
       take(1),
       catchError((e: HttpErrorResponse) => {
         if (e.status === 404) {
-          return 'error';
+          return of('error');
         }
         return throwError(() => e);
       }),
