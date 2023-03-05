387580eafac270f9320679f604be59cfee6d667f
annah <anna.h.domanska@gmail.com>
2023-03-05 13:38:23 +0100

    feat: refresh and access Token subjects added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { CredentialsModel } from '../models/credentials.model';
 import { CredentialsResponse } from '../responses/credentials.response';
 import { ApiResponse } from '../responses/api.response';
@@ -9,7 +9,16 @@ import { RegisterUserResponse } from '../responses/register-user.response';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private _httpClient: HttpClient) {}
+  private _accessTokenSubject: BehaviorSubject<string | null> =
+    new BehaviorSubject<string | null>(this._storage.getItem('accessToken'));
+  public accessToken$: Observable<string | null> =
+    this._accessTokenSubject.asObservable();
+  private _refreshTokenSubject: BehaviorSubject<string | null> =
+    new BehaviorSubject<string | null>(this._storage.getItem('refreshToken'));
+  public refreshToken$: Observable<string | null> =
+    this._refreshTokenSubject.asObservable();
+
+  constructor(private _httpClient: HttpClient, private _storage: Storage) {}
 
   login(loginData: CredentialsModel): Observable<CredentialsResponse> {
     return this._httpClient
@@ -17,7 +26,14 @@ export class AuthService {
         `https://us-central1-courses-auth.cloudfunctions.net/auth/login`,
         { data: loginData }
       )
-      .pipe(map((resp) => resp.data));
+      .pipe(
+        map((resp) => resp.data),
+        tap((data) => {
+          this._accessTokenSubject.next(data.accessToken);
+          this._refreshTokenSubject.next(data.refreshToken);
+      
+        })
+      );
   }
 
   register(registerData: CredentialsModel): Observable<RegisterUserResponse> {
@@ -27,5 +43,10 @@ export class AuthService {
         { data: registerData }
       )
       .pipe(map((resp) => resp.data));
+  }
+
+  setUserToStorage(userData: CredentialsResponse): void {
+    this._storage.setItem('accessToken', userData.accessToken);
+    this._storage.setItem('refreshToken', userData.refreshToken);
   }
 }
