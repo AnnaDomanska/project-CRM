ed7263a719e398662da697b45da87edb4f154eb9
annah <anna.h.domanska@gmail.com>
2023-03-05 21:17:34 +0100

    feat: leads component added to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { VerifyComponent } from './components/verify/verify.component';
+import { LeadsComponent } from './components/leads/leads.component';
 import { LoginComponentModule } from './components/login/login.component-module';
 import { RegisterComponentModule } from './components/register/register.component-module';
 import { VerifyComponentModule } from './components/verify/verify.component-module';
+import { LeadsComponentModule } from './components/leads/leads.component-module';
 
 @NgModule({
   imports: [
@@ -13,11 +15,13 @@ import { VerifyComponentModule } from './components/verify/verify.component-modu
       { path: 'auth/login', component: LoginComponent },
       { path: 'auth/register', component: RegisterComponent },
       { path: 'verify', component: VerifyComponent },
+      { path: 'leads', component: LeadsComponent }
     ]),
     LoginComponentModule,
     RegisterComponentModule,
     VerifyComponentModule,
+    LeadsComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
