23056203c94cd340ab5dbed59d484f4831c6c93b
annah <anna.h.domanska@gmail.com>
2023-03-06 22:13:46 +0100

    feat: not-logged-in guard implemented

--- src/app/guards/not-logged-in/not-logged-in.guard.ts
+++ src/app/guards/not-logged-in/not-logged-in.guard.ts
@@ -1,5 +1,26 @@
 import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { map, Observable, take } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Injectable({ providedIn: 'root' })
-export class NotLoggedInGuard {
+export class NotLoggedInGuard implements CanActivate {
+  constructor(private _userService: UserService, private _router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> {
+    return this._userService.getUserData().pipe(
+      take(1),
+      map((userData) => {
+        return userData ? true : this._router.parseUrl('auth/login');
+      })
+    );
+  }
 }
