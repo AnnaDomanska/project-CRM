1aa287f2882ebb9c95613a7f5e084766570403c8
annah <anna.h.domanska@gmail.com>
2023-03-06 22:08:30 +0100

    feat: auto-login guard implemented

--- src/app/guards/auto-login/auto-login.guard.ts
+++ src/app/guards/auto-login/auto-login.guard.ts
@@ -1,5 +1,26 @@
 import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, take, map } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Injectable({ providedIn: 'root' })
-export class AutoLoginGuard {
+export class AutoLoginGuard implements CanActivate {
+  constructor(private _userService: UserService, private _router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> {
+    return this._userService.getUserData().pipe(
+      take(1),
+      map((userData) => {
+        return userData ? true : this._router.parseUrl('auth/login');
+      })
+    );
+  }
 }
