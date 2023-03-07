5edb40bcef412a165ee63ae428b4bce65504d7f5
annah <anna.h.domanska@gmail.com>
2023-03-07 22:14:48 +0100

    feat: dropdown menu added to leads component

--- src/app/components/leads/leads.component.ts
+++ src/app/components/leads/leads.component.ts
@@ -1,11 +1,24 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-leads',
   styleUrls: ['./leads.component.scss'],
   templateUrl: './leads.component.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeadsComponent {
+  private _dropdownMenuStatusSubject: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(false);
+  public dropdownMenuStatus$: Observable<boolean> =
+    this._dropdownMenuStatusSubject.asObservable();
+
+  showMenu(value: boolean): void {
+    this._dropdownMenuStatusSubject.next(value);
+  }
 }
