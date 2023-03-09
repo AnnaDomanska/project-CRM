baf3bd846cabd39c93d923ccd54ebc4b891676e4
annah <anna.h.domanska@gmail.com>
2023-03-09 15:01:09 +0100

    feat: createLeadForm added

--- src/app/components/create-lead/create-lead.component.ts
+++ src/app/components/create-lead/create-lead.component.ts
@@ -1,11 +1,30 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-lead',
   styleUrls: ['./create-lead.component.scss'],
   templateUrl: './create-lead.component.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateLeadComponent {
+  readonly createLeadForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    websiteLink: new FormControl(),
+    location: new FormControl(),
+    industry: new FormControl(),
+    annualRevenue: new FormControl(),
+    activities: new FormControl(),
+    totalSize: new FormControl(),
+    devSize: new FormControl(),
+    feSize: new FormControl(),
+    hiring: new FormControl(),
+    status: new FormControl(),
+    notes: new FormControl(),
+  });
 }
