c010e5d1d4d0f430f09d80efb8757a78c41f138f
annah <anna.h.domanska@gmail.com>
2023-03-09 16:06:43 +0100

    feat: create lead buttons disabled

--- src/app/components/create-lead/create-lead.component-module.ts
+++ src/app/components/create-lead/create-lead.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateLeadComponent } from './create-lead.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   declarations: [CreateLeadComponent],
   providers: [],
   exports: [CreateLeadComponent],
