c16c54afc13a7247642d478ad8bbe7b502c935d7
annah <anna.h.domanska@gmail.com>
2023-03-09 14:54:02 +0100

    feat: router link added to create lead button

--- src/app/components/leads/leads.component-module.ts
+++ src/app/components/leads/leads.component-module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { LeadsComponent } from './leads.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   declarations: [LeadsComponent],
   providers: [],
   exports: [LeadsComponent],
