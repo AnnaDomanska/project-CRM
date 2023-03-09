42ac64295395b8f7a8de29b8e8f088be6f256c64
annah <anna.h.domanska@gmail.com>
2023-03-09 12:18:23 +0100

    feat: leads query model added

--- src/app/query-models/leads.query-model.ts
+++ src/app/query-models/leads.query-model.ts
@@ -1,4 +1,17 @@
 export interface LeadsQueryModel {
   readonly name: string;
-  
+  readonly scopes: string[];
+  readonly hiring: {
+    readonly active: boolean;
+    readonly junior: boolean;
+    readonly talentProgram: boolean;
+  };
+  readonly industry: string;
+  readonly location: string;
+  readonly size: {
+    readonly total: number;
+    readonly dev: number;
+    readonly fe: number;
+  };
+  readonly revenue: number;
 }
