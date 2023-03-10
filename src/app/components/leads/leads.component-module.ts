eac48f283e6ec0bf81e7963fd08e14e5fc7eb3dd
annah <anna.h.domanska@gmail.com>
2023-03-10 22:09:25 +0100

    feat: router outlet added to leads component

--- src/app/components/leads/leads.component-module.ts
+++ src/app/components/leads/leads.component-module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { LeadsComponent } from './leads.component';
 
 @NgModule({
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterModule],
   declarations: [LeadsComponent],
   providers: [],
   exports: [LeadsComponent],
