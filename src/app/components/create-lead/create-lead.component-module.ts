355ae6c3f4151f81ab889ed5d751389b1c48ff60
annah <anna.h.domanska@gmail.com>
2023-03-12 22:56:02 +0100

    feat: size erros pipe implemented to create lead component

--- src/app/components/create-lead/create-lead.component-module.ts
+++ src/app/components/create-lead/create-lead.component-module.ts
@@ -3,9 +3,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CreateLeadComponent } from './create-lead.component';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { SizeErrorsPipe } from 'src/app/pipes/size-errors.pipe';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, SizeErrorsPipe],
   declarations: [CreateLeadComponent],
   providers: [],
   exports: [CreateLeadComponent],
