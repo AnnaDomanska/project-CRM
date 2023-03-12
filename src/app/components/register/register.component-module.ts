c0b8262165eaff691f6005aa4eacd13142904425
annah <anna.h.domanska@gmail.com>
2023-03-12 22:44:05 +0100

    feat: pipes implemented in register component

--- src/app/components/register/register.component-module.ts
+++ src/app/components/register/register.component-module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EmailErrorsPipe } from 'src/app/pipes/email-errors.pipe';
+import { PasswordErrorsPipe } from 'src/app/pipes/password-errors.pipe';
 import { RegisterComponent } from './register.component';
 
 @NgModule({
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    EmailErrorsPipe,
+    PasswordErrorsPipe,
+  ],
   declarations: [RegisterComponent],
   providers: [],
   exports: [RegisterComponent],
